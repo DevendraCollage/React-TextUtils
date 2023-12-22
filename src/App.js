51e51625c9052fdc015414a5b4257b7576eb8ea4
Devendra Parmar <devendraparmarcollage@gmail.com>
2023-12-22 18:48:19 +0530

    modified:   src/App.js 	modified:   src/Components/Navbar.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
 import Alert from "./Components/Alert";
 import Footer from "./Components/Footer";
-import About from "./Components/About";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); //? Whether dark mode is enabled or not
@@ -36,33 +34,22 @@ function App() {
 
   return (
     <>
-      <Router>
-        <Navbar
-          title="TextUtils"
-          about="About Text Utils"
+      <Navbar
+        title="TextUtils"
+        about="About Text Utils"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
+      {/* Props About and Title Passed as a variable */}
+      <div className="container">
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter The Text To Analyze Below"
           mode={mode}
-          toggleMode={toggleMode}
         />
-        <Alert alert={alert} />
-        {/* Props About and Title Passed as a variable */}
-        <div className="container">
-          <Routes>
-            <Route path="/about" element={<About mode={mode} />} />
-            <Route
-              exact
-              path="/"
-              element={
-                <TextForm
-                  showAlert={showAlert}
-                  heading="Enter The Text To Analyze Below"
-                  mode={mode}
-                />
-              }
-            />
-          </Routes>
-        </div>
-        <Footer />
-      </Router>
+      </div>
+      <Footer />
     </>
   );
 }
